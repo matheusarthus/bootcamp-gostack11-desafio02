2a79c75a4984e7ae27190513064f3adb0abbe79a
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-06-16 15:09:52 -0300

    feature: GET /repositories added

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
